c04d419ef5c34b8208a0b2d4660d104afa3f67b8
Diego Sanchez Franco <diego.sanchez@ivaq.es>
2026-09-08 15:58:11 +0200

    refactor: align mini capture command naming

--- generated/include/mavlink/v2.0/ivaq_rx_mini/ivaq_rx_mini/mavlink_msg_ivaq_rx_mini_set_params.h
+++ generated/include/mavlink/v2.0/ivaq_rx_mini/ivaq_rx_mini/mavlink_msg_ivaq_rx_mini_set_params.h
@@ -7,7 +7,7 @@
 typedef struct __mavlink_ivaq_rx_mini_set_params_t {
  uint8_t rx_set_update; /*<  Request receiver firmware update mode.*/
  uint8_t rx_set_reset; /*<  Request receiver reset.*/
- uint8_t rx_capture_command; /*<  Requested acquisition/transfer action.*/
+ uint8_t rx_set_capture; /*<  Requested acquisition/transfer action.*/
 } mavlink_ivaq_rx_mini_set_params_t;
 
 #define MAVLINK_MSG_ID_IVAQ_RX_MINI_SET_PARAMS_LEN 3
@@ -15,8 +15,8 @@ typedef struct __mavlink_ivaq_rx_mini_set_params_t {
 #define MAVLINK_MSG_ID_61001_LEN 3
 #define MAVLINK_MSG_ID_61001_MIN_LEN 3
 
-#define MAVLINK_MSG_ID_IVAQ_RX_MINI_SET_PARAMS_CRC 228
-#define MAVLINK_MSG_ID_61001_CRC 228
+#define MAVLINK_MSG_ID_IVAQ_RX_MINI_SET_PARAMS_CRC 24
+#define MAVLINK_MSG_ID_61001_CRC 24
 
 
 
@@ -27,7 +27,7 @@ typedef struct __mavlink_ivaq_rx_mini_set_params_t {
     3, \
     {  { "rx_set_update", NULL, MAVLINK_TYPE_UINT8_T, 0, 0, offsetof(mavlink_ivaq_rx_mini_set_params_t, rx_set_update) }, \
          { "rx_set_reset", NULL, MAVLINK_TYPE_UINT8_T, 0, 1, offsetof(mavlink_ivaq_rx_mini_set_params_t, rx_set_reset) }, \
-         { "rx_capture_command", NULL, MAVLINK_TYPE_UINT8_T, 0, 2, offsetof(mavlink_ivaq_rx_mini_set_params_t, rx_capture_command) }, \
+         { "rx_set_capture", NULL, MAVLINK_TYPE_UINT8_T, 0, 2, offsetof(mavlink_ivaq_rx_mini_set_params_t, rx_set_capture) }, \
          } \
 }
 #else
@@ -36,7 +36,7 @@ typedef struct __mavlink_ivaq_rx_mini_set_params_t {
     3, \
     {  { "rx_set_update", NULL, MAVLINK_TYPE_UINT8_T, 0, 0, offsetof(mavlink_ivaq_rx_mini_set_params_t, rx_set_update) }, \
          { "rx_set_reset", NULL, MAVLINK_TYPE_UINT8_T, 0, 1, offsetof(mavlink_ivaq_rx_mini_set_params_t, rx_set_reset) }, \
-         { "rx_capture_command", NULL, MAVLINK_TYPE_UINT8_T, 0, 2, offsetof(mavlink_ivaq_rx_mini_set_params_t, rx_capture_command) }, \
+         { "rx_set_capture", NULL, MAVLINK_TYPE_UINT8_T, 0, 2, offsetof(mavlink_ivaq_rx_mini_set_params_t, rx_set_capture) }, \
          } \
 }
 #endif
@@ -49,24 +49,24 @@ typedef struct __mavlink_ivaq_rx_mini_set_params_t {
  *
  * @param rx_set_update  Request receiver firmware update mode.
  * @param rx_set_reset  Request receiver reset.
- * @param rx_capture_command  Requested acquisition/transfer action.
+ * @param rx_set_capture  Requested acquisition/transfer action.
  * @return length of the message in bytes (excluding serial stream start sign)
  */
 static inline uint16_t mavlink_msg_ivaq_rx_mini_set_params_pack(uint8_t system_id, uint8_t component_id, mavlink_message_t* msg,
-                               uint8_t rx_set_update, uint8_t rx_set_reset, uint8_t rx_capture_command)
+                               uint8_t rx_set_update, uint8_t rx_set_reset, uint8_t rx_set_capture)
 {
 #if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
     char buf[MAVLINK_MSG_ID_IVAQ_RX_MINI_SET_PARAMS_LEN];
     _mav_put_uint8_t(buf, 0, rx_set_update);
     _mav_put_uint8_t(buf, 1, rx_set_reset);
-    _mav_put_uint8_t(buf, 2, rx_capture_command);
+    _mav_put_uint8_t(buf, 2, rx_set_capture);
 
         memcpy(_MAV_PAYLOAD_NON_CONST(msg), buf, MAVLINK_MSG_ID_IVAQ_RX_MINI_SET_PARAMS_LEN);
 #else
     mavlink_ivaq_rx_mini_set_params_t packet;
     packet.rx_set_update = rx_set_update;
     packet.rx_set_reset = rx_set_reset;
-    packet.rx_capture_command = rx_capture_command;
+    packet.rx_set_capture = rx_set_capture;
 
         memcpy(_MAV_PAYLOAD_NON_CONST(msg), &packet, MAVLINK_MSG_ID_IVAQ_RX_MINI_SET_PARAMS_LEN);
 #endif
@@ -84,24 +84,24 @@ static inline uint16_t mavlink_msg_ivaq_rx_mini_set_params_pack(uint8_t system_i
  *
  * @param rx_set_update  Request receiver firmware update mode.
  * @param rx_set_reset  Request receiver reset.
- * @param rx_capture_command  Requested acquisition/transfer action.
+ * @param rx_set_capture  Requested acquisition/transfer action.
  * @return length of the message in bytes (excluding serial stream start sign)
  */
 static inline uint16_t mavlink_msg_ivaq_rx_mini_set_params_pack_status(uint8_t system_id, uint8_t component_id, mavlink_status_t *_status, mavlink_message_t* msg,
-                               uint8_t rx_set_update, uint8_t rx_set_reset, uint8_t rx_capture_command)
+                               uint8_t rx_set_update, uint8_t rx_set_reset, uint8_t rx_set_capture)
 {
 #if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
     char buf[MAVLINK_MSG_ID_IVAQ_RX_MINI_SET_PARAMS_LEN];
     _mav_put_uint8_t(buf, 0, rx_set_update);
     _mav_put_uint8_t(buf, 1, rx_set_reset);
-    _mav_put_uint8_t(buf, 2, rx_capture_command);
+    _mav_put_uint8_t(buf, 2, rx_set_capture);
 
         memcpy(_MAV_PAYLOAD_NON_CONST(msg), buf, MAVLINK_MSG_ID_IVAQ_RX_MINI_SET_PARAMS_LEN);
 #else
     mavlink_ivaq_rx_mini_set_params_t packet;
     packet.rx_set_update = rx_set_update;
     packet.rx_set_reset = rx_set_reset;
-    packet.rx_capture_command = rx_capture_command;
+    packet.rx_set_capture = rx_set_capture;
 
         memcpy(_MAV_PAYLOAD_NON_CONST(msg), &packet, MAVLINK_MSG_ID_IVAQ_RX_MINI_SET_PARAMS_LEN);
 #endif
@@ -122,25 +122,25 @@ static inline uint16_t mavlink_msg_ivaq_rx_mini_set_params_pack_status(uint8_t s
  * @param msg The MAVLink message to compress the data into
  * @param rx_set_update  Request receiver firmware update mode.
  * @param rx_set_reset  Request receiver reset.
- * @param rx_capture_command  Requested acquisition/transfer action.
+ * @param rx_set_capture  Requested acquisition/transfer action.
  * @return length of the message in bytes (excluding serial stream start sign)
  */
 static inline uint16_t mavlink_msg_ivaq_rx_mini_set_params_pack_chan(uint8_t system_id, uint8_t component_id, uint8_t chan,
                                mavlink_message_t* msg,
-                                   uint8_t rx_set_update,uint8_t rx_set_reset,uint8_t rx_capture_command)
+                                   uint8_t rx_set_update,uint8_t rx_set_reset,uint8_t rx_set_capture)
 {
 #if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
     char buf[MAVLINK_MSG_ID_IVAQ_RX_MINI_SET_PARAMS_LEN];
     _mav_put_uint8_t(buf, 0, rx_set_update);
     _mav_put_uint8_t(buf, 1, rx_set_reset);
-    _mav_put_uint8_t(buf, 2, rx_capture_command);
+    _mav_put_uint8_t(buf, 2, rx_set_capture);
 
         memcpy(_MAV_PAYLOAD_NON_CONST(msg), buf, MAVLINK_MSG_ID_IVAQ_RX_MINI_SET_PARAMS_LEN);
 #else
     mavlink_ivaq_rx_mini_set_params_t packet;
     packet.rx_set_update = rx_set_update;
     packet.rx_set_reset = rx_set_reset;
-    packet.rx_capture_command = rx_capture_command;
+    packet.rx_set_capture = rx_set_capture;
 
         memcpy(_MAV_PAYLOAD_NON_CONST(msg), &packet, MAVLINK_MSG_ID_IVAQ_RX_MINI_SET_PARAMS_LEN);
 #endif
@@ -159,7 +159,7 @@ static inline uint16_t mavlink_msg_ivaq_rx_mini_set_params_pack_chan(uint8_t sys
  */
 static inline uint16_t mavlink_msg_ivaq_rx_mini_set_params_encode(uint8_t system_id, uint8_t component_id, mavlink_message_t* msg, const mavlink_ivaq_rx_mini_set_params_t* ivaq_rx_mini_set_params)
 {
-    return mavlink_msg_ivaq_rx_mini_set_params_pack(system_id, component_id, msg, ivaq_rx_mini_set_params->rx_set_update, ivaq_rx_mini_set_params->rx_set_reset, ivaq_rx_mini_set_params->rx_capture_command);
+    return mavlink_msg_ivaq_rx_mini_set_params_pack(system_id, component_id, msg, ivaq_rx_mini_set_params->rx_set_update, ivaq_rx_mini_set_params->rx_set_reset, ivaq_rx_mini_set_params->rx_set_capture);
 }
 
 /**
@@ -173,7 +173,7 @@ static inline uint16_t mavlink_msg_ivaq_rx_mini_set_params_encode(uint8_t system
  */
 static inline uint16_t mavlink_msg_ivaq_rx_mini_set_params_encode_chan(uint8_t system_id, uint8_t component_id, uint8_t chan, mavlink_message_t* msg, const mavlink_ivaq_rx_mini_set_params_t* ivaq_rx_mini_set_params)
 {
-    return mavlink_msg_ivaq_rx_mini_set_params_pack_chan(system_id, component_id, chan, msg, ivaq_rx_mini_set_params->rx_set_update, ivaq_rx_mini_set_params->rx_set_reset, ivaq_rx_mini_set_params->rx_capture_command);
+    return mavlink_msg_ivaq_rx_mini_set_params_pack_chan(system_id, component_id, chan, msg, ivaq_rx_mini_set_params->rx_set_update, ivaq_rx_mini_set_params->rx_set_reset, ivaq_rx_mini_set_params->rx_set_capture);
 }
 
 /**
@@ -187,7 +187,7 @@ static inline uint16_t mavlink_msg_ivaq_rx_mini_set_params_encode_chan(uint8_t s
  */
 static inline uint16_t mavlink_msg_ivaq_rx_mini_set_params_encode_status(uint8_t system_id, uint8_t component_id, mavlink_status_t* _status, mavlink_message_t* msg, const mavlink_ivaq_rx_mini_set_params_t* ivaq_rx_mini_set_params)
 {
-    return mavlink_msg_ivaq_rx_mini_set_params_pack_status(system_id, component_id, _status, msg,  ivaq_rx_mini_set_params->rx_set_update, ivaq_rx_mini_set_params->rx_set_reset, ivaq_rx_mini_set_params->rx_capture_command);
+    return mavlink_msg_ivaq_rx_mini_set_params_pack_status(system_id, component_id, _status, msg,  ivaq_rx_mini_set_params->rx_set_update, ivaq_rx_mini_set_params->rx_set_reset, ivaq_rx_mini_set_params->rx_set_capture);
 }
 
 /**
@@ -196,24 +196,24 @@ static inline uint16_t mavlink_msg_ivaq_rx_mini_set_params_encode_status(uint8_t
  *
  * @param rx_set_update  Request receiver firmware update mode.
  * @param rx_set_reset  Request receiver reset.
- * @param rx_capture_command  Requested acquisition/transfer action.
+ * @param rx_set_capture  Requested acquisition/transfer action.
  */
 #ifdef MAVLINK_USE_CONVENIENCE_FUNCTIONS
 
-static inline void mavlink_msg_ivaq_rx_mini_set_params_send(mavlink_channel_t chan, uint8_t rx_set_update, uint8_t rx_set_reset, uint8_t rx_capture_command)
+static inline void mavlink_msg_ivaq_rx_mini_set_params_send(mavlink_channel_t chan, uint8_t rx_set_update, uint8_t rx_set_reset, uint8_t rx_set_capture)
 {
 #if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
     char buf[MAVLINK_MSG_ID_IVAQ_RX_MINI_SET_PARAMS_LEN];
     _mav_put_uint8_t(buf, 0, rx_set_update);
     _mav_put_uint8_t(buf, 1, rx_set_reset);
-    _mav_put_uint8_t(buf, 2, rx_capture_command);
+    _mav_put_uint8_t(buf, 2, rx_set_capture);
 
     _mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_IVAQ_RX_MINI_SET_PARAMS, buf, MAVLINK_MSG_ID_IVAQ_RX_MINI_SET_PARAMS_MIN_LEN, MAVLINK_MSG_ID_IVAQ_RX_MINI_SET_PARAMS_LEN, MAVLINK_MSG_ID_IVAQ_RX_MINI_SET_PARAMS_CRC);
 #else
     mavlink_ivaq_rx_mini_set_params_t packet;
     packet.rx_set_update = rx_set_update;
     packet.rx_set_reset = rx_set_reset;
-    packet.rx_capture_command = rx_capture_command;
+    packet.rx_set_capture = rx_set_capture;
 
     _mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_IVAQ_RX_MINI_SET_PARAMS, (const char *)&packet, MAVLINK_MSG_ID_IVAQ_RX_MINI_SET_PARAMS_MIN_LEN, MAVLINK_MSG_ID_IVAQ_RX_MINI_SET_PARAMS_LEN, MAVLINK_MSG_ID_IVAQ_RX_MINI_SET_PARAMS_CRC);
 #endif
@@ -227,7 +227,7 @@ static inline void mavlink_msg_ivaq_rx_mini_set_params_send(mavlink_channel_t ch
 static inline void mavlink_msg_ivaq_rx_mini_set_params_send_struct(mavlink_channel_t chan, const mavlink_ivaq_rx_mini_set_params_t* ivaq_rx_mini_set_params)
 {
 #if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
-    mavlink_msg_ivaq_rx_mini_set_params_send(chan, ivaq_rx_mini_set_params->rx_set_update, ivaq_rx_mini_set_params->rx_set_reset, ivaq_rx_mini_set_params->rx_capture_command);
+    mavlink_msg_ivaq_rx_mini_set_params_send(chan, ivaq_rx_mini_set_params->rx_set_update, ivaq_rx_mini_set_params->rx_set_reset, ivaq_rx_mini_set_params->rx_set_capture);
 #else
     _mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_IVAQ_RX_MINI_SET_PARAMS, (const char *)ivaq_rx_mini_set_params, MAVLINK_MSG_ID_IVAQ_RX_MINI_SET_PARAMS_MIN_LEN, MAVLINK_MSG_ID_IVAQ_RX_MINI_SET_PARAMS_LEN, MAVLINK_MSG_ID_IVAQ_RX_MINI_SET_PARAMS_CRC);
 #endif
@@ -241,20 +241,20 @@ static inline void mavlink_msg_ivaq_rx_mini_set_params_send_struct(mavlink_chann
   is usually the receive buffer for the channel, and allows a reply to an
   incoming message with minimum stack space usage.
  */
-static inline void mavlink_msg_ivaq_rx_mini_set_params_send_buf(mavlink_message_t *msgbuf, mavlink_channel_t chan,  uint8_t rx_set_update, uint8_t rx_set_reset, uint8_t rx_capture_command)
+static inline void mavlink_msg_ivaq_rx_mini_set_params_send_buf(mavlink_message_t *msgbuf, mavlink_channel_t chan,  uint8_t rx_set_update, uint8_t rx_set_reset, uint8_t rx_set_capture)
 {
 #if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
     char *buf = (char *)msgbuf;
     _mav_put_uint8_t(buf, 0, rx_set_update);
     _mav_put_uint8_t(buf, 1, rx_set_reset);
-    _mav_put_uint8_t(buf, 2, rx_capture_command);
+    _mav_put_uint8_t(buf, 2, rx_set_capture);
 
     _mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_IVAQ_RX_MINI_SET_PARAMS, buf, MAVLINK_MSG_ID_IVAQ_RX_MINI_SET_PARAMS_MIN_LEN, MAVLINK_MSG_ID_IVAQ_RX_MINI_SET_PARAMS_LEN, MAVLINK_MSG_ID_IVAQ_RX_MINI_SET_PARAMS_CRC);
 #else
     mavlink_ivaq_rx_mini_set_params_t *packet = (mavlink_ivaq_rx_mini_set_params_t *)msgbuf;
     packet->rx_set_update = rx_set_update;
     packet->rx_set_reset = rx_set_reset;
-    packet->rx_capture_command = rx_capture_command;
+    packet->rx_set_capture = rx_set_capture;
 
     _mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_IVAQ_RX_MINI_SET_PARAMS, (const char *)packet, MAVLINK_MSG_ID_IVAQ_RX_MINI_SET_PARAMS_MIN_LEN, MAVLINK_MSG_ID_IVAQ_RX_MINI_SET_PARAMS_LEN, MAVLINK_MSG_ID_IVAQ_RX_MINI_SET_PARAMS_CRC);
 #endif
@@ -287,11 +287,11 @@ static inline uint8_t mavlink_msg_ivaq_rx_mini_set_params_get_rx_set_reset(const
 }
 
 /**
- * @brief Get field rx_capture_command from ivaq_rx_mini_set_params message
+ * @brief Get field rx_set_capture from ivaq_rx_mini_set_params message
  *
  * @return  Requested acquisition/transfer action.
  */
-static inline uint8_t mavlink_msg_ivaq_rx_mini_set_params_get_rx_capture_command(const mavlink_message_t* msg)
+static inline uint8_t mavlink_msg_ivaq_rx_mini_set_params_get_rx_set_capture(const mavlink_message_t* msg)
 {
     return _MAV_RETURN_uint8_t(msg,  2);
 }
@@ -307,7 +307,7 @@ static inline void mavlink_msg_ivaq_rx_mini_set_params_decode(const mavlink_mess
 #if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
     ivaq_rx_mini_set_params->rx_set_update = mavlink_msg_ivaq_rx_mini_set_params_get_rx_set_update(msg);
     ivaq_rx_mini_set_params->rx_set_reset = mavlink_msg_ivaq_rx_mini_set_params_get_rx_set_reset(msg);
-    ivaq_rx_mini_set_params->rx_capture_command = mavlink_msg_ivaq_rx_mini_set_params_get_rx_capture_command(msg);
+    ivaq_rx_mini_set_params->rx_set_capture = mavlink_msg_ivaq_rx_mini_set_params_get_rx_set_capture(msg);
 #else
         uint8_t len = msg->len < MAVLINK_MSG_ID_IVAQ_RX_MINI_SET_PARAMS_LEN? msg->len : MAVLINK_MSG_ID_IVAQ_RX_MINI_SET_PARAMS_LEN;
         memset(ivaq_rx_mini_set_params, 0, MAVLINK_MSG_ID_IVAQ_RX_MINI_SET_PARAMS_LEN);
--- generated/include/mavlink/v2.0/ivaq_rx_mini/minimal/minimal.h
+++ generated/include/mavlink/v2.0/ivaq_rx_mini/minimal/minimal.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_MINIMAL.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_MINIMAL_XML_HASH 5542590353318892208
+#define MAVLINK_MINIMAL_XML_HASH -5032357390880484606
 
 #ifdef __cplusplus
 extern "C" {
